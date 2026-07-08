d950a2cdb9ceb56ff4737ad7b35316f6b07cd4c3
Vincent von Behren <vonbehrenv@gmail.com>
2026-07-08 22:01:08 +0200

    Ortsteil Feldheim und Neuigkeiten

--- public/js/app.js
+++ public/js/app.js
@@ -116,7 +116,7 @@ function setAccount(acc, token) {
 }
 
 // ---- Update-/Changelog-Modal (einmal pro Version) ----
-const UPDATE_VERSION = "2026-07-06-level-clans-wheel";
+const UPDATE_VERSION = "2026-07-08-feldheim-jobs-social";
 function maybeShowUpdate() {
   let seen = null;
   try { seen = localStorage.getItem("casino_seen_update"); } catch {}
