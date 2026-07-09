89ba4c98ee10d26c6e082e8e9717a4304c80f970
Vincent von Behren <vonbehrenv@gmail.com>
2026-07-09 14:19:39 +0200

    Wechselnde kleine Aufgaben beim Arbeiten

--- public/js/app.js
+++ public/js/app.js
@@ -118,7 +118,7 @@ function setAccount(acc, token) {
 }
 
 // ---- Update-/Changelog-Modal (einmal pro Version) ----
-const UPDATE_VERSION = "2026-07-09-work-tasks-level-board";
+const UPDATE_VERSION = "2026-07-09-work-task-rotation";
 function maybeShowUpdate() {
   let seen = null;
   try { seen = localStorage.getItem("casino_seen_update"); } catch {}
